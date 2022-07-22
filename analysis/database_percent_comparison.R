0021759d0b9d93b1aa87decef933573ed94f1166
mariewcheng <marie.cheng@duke.edu>
2022-07-22 18:55:25 -0400

    updated

--- analysis/database_percent_comparison.R
+++ analysis/database_percent_comparison.R
@@ -10,6 +10,7 @@
 
 library(ggplot2)
 library(data.table)
+library(ggpubr)
 
 ########################################################################
 # IMPORTANT: Experiment-dependent variables below, need to be set ...
@@ -32,23 +33,76 @@ make_subset_across_plot <- function(group_df, name) {
   setDF(group_df_melt)
   
   # Build the stacked bar chart ...
-  graph_title <- paste("Uniqueness of Kmers Across All", length(dataset_names), "Groups w.r.t", name, "Pivot")
   plot <- ggplot(group_df_melt, aes(fill=range, x=database_size, y=percent)) + 
     geom_bar(position="fill", stat="identity") +
     theme_classic() +
-    theme(plot.title=element_text(hjust = 0.5, size=14, face="bold"),
+    theme(plot.title=element_text(hjust = 0.5, size=14),
           axis.title.x=element_text(size =14),
           axis.title.y=element_text(size=14),
           legend.position = "bottom", 
-          legend.text=element_text(size=10),
-          legend.box="vertical",
-          legend.title=element_text(size=10),
-          axis.text=element_text(size=14, color="black")) +
-    labs(x="Database Size",
+          legend.text=element_text(size=14),
+          legend.box="horizontal",
+          legend.title=element_text(size=14),
+          axis.text=element_text(size=12, color="black")) +
+    scale_x_continuous(breaks = seq(20, 100, 20)) +
+    labs(x="Database Size (% RefSeq)",
          y="Ratio of Unique Kmers",
-         title=graph_title) +
+         title=name) +
     scale_fill_discrete(name = "Number of Groups the Kmers Occur In:",
                         labels = c("4 Groups", "2 to 3 Groups", "Only Pivot"))
+  return (plot)
+}
+
+make_subset_unique_across_plot <- function(group_df, name) {
+  # Convert to data.table, melt, and then revert to data.frame
+  
+  # Build the stacked bar chart ...
+  plot <- ggplot(group_df, aes(x=database_size, y=percent_1_occ)) + 
+    geom_bar(stat="identity",fill="steelblue") +
+    theme_classic() +
+    theme(plot.title=element_text(hjust = 0.5, size=14),
+          axis.title.x=element_text(size =14),
+          axis.title.y=element_text(size=14),
+          legend.position = "bottom", 
+          legend.text=element_text(size=14),
+          legend.box="horizontal",
+          legend.title=element_text(size=14),
+          axis.text=element_text(size=12, color="black")) +
+    scale_x_continuous(breaks = seq(20, 100, 20)) +
+    labs(x="Database Size (% RefSeq)",
+         y="% Kmers Unique to Pivot",
+         title=name) 
+  return (plot)
+}
+
+make_across_groups_plot <- function(group_df, name) {
+  # Convert to data.table, melt, and then revert to data.frame
+  setDT(group_df)
+  group_df_melt <- melt(group_df, 
+                        measure.vars = c("percent_4_to_8","percent_2_to_3", "percent_1_occ"),
+                        variable.name = "range", 
+                        value.name = "percent")
+  print(tail(group_df_melt))
+  setDF(group_df_melt)
+  
+  # Build the stacked bar chart ...
+  plot <- ggplot(group_df_melt, aes(fill=range, x=factor(k), y=percent)) + 
+    geom_bar(position="fill", stat="identity") +
+    theme_classic() +
+    theme(plot.title=element_text(hjust = 0.5, size=14),
+          axis.title.x=element_text(size =14),
+          axis.title.y=element_text(size=14),
+          legend.position = "bottom", 
+          legend.text=element_text(size=14),
+          legend.box="horizontal",
+          legend.title=element_text(size=14),
+          axis.text=element_text(size=12, color="black")) +
+    labs(x="Kmer Length (k)",
+         y="Ratio of Unique Kmers",
+         title=name) +
+    scale_fill_discrete(name = "Number of Groups the Kmers Occur In: ",
+                        labels = c("4 Groups", "2 to 3 Groups", "Only Pivot")) +
+    scale_x_discrete(breaks = seq(7, 35, 4)) 
   return (plot)
 }
 
@@ -61,30 +115,64 @@ make_subset_across_plot <- function(group_df, name) {
 #######################################################################
 
 percent_files <- list.files(path=working_dir,pattern = "\\.csv$")
-col_names <- c("k","dataset","TP","TN","FP","FN")
 
 percent_df_list = list()
 
 for(i in seq_along(percent_files)){
   print(percent_files[i])
   temp_df = read.csv(file = paste(working_dir,percent_files[i], sep = ""),header = TRUE)
+  temp_df[,"database_size"] <- strtoi(strsplit(strsplit(percent_files[i], "_")[[1]],".csv")[[2]])
   percent_df_list[[percent_files[i]]] <- temp_df
 }
+merge_df <- Reduce(function(x, y) merge(x, y, all=TRUE), percent_df_list)
 
-percentages = c("20","40","60","80","100")
-print(strsplit("trial_100.csv", "_")[1])
-strsplit(strsplit("trial_100.csv", "_")[[1]],".csv")[[2]]
+###################################
+# GENERATE PLOTS 
+###################################
 
-anthracis_subset_list = list()
+### Arrange 4 species across k at 20 and 100%
+percent_db = 20
+
+plot_list = list()
+for(dataset in 0:(length(dataset_names)-1)){
+  group = paste("group_",dataset+1, sep = "")
+  temp_df = merge_df[merge_df$group_num == group, ]
+  temp_df = temp_df[temp_df$database_size == percent_db, ]
+  temp_plot <-make_across_groups_plot(temp_df,dataset_names[dataset+1])
+  plot_list[[dataset + 1]]<- temp_plot
+}
+
+final_plot <- ggarrange(plot_list[[1]], plot_list[[2]], plot_list[[3]], plot_list[[4]],
+                        labels = c("a", "b", "c", "d"), ncol = 2, nrow = 2,
+                        common.legend = TRUE, legend = "bottom")
+
+
+print(final_plot)
+output_name <- paste(working_dir, "bacillus_database_20.jpeg", sep="")
+ggsave(output_name, plot=final_plot, dpi=800, device="jpeg", width=10, height=8)
+
+output_name <- paste(working_dir, "bacillus_database_20.pdf", sep="")
+ggsave(output_name, plot=final_plot, dpi=800, device="pdf", width=10, height=8)
+
+### Subset for species and k over sizes
+
+subset_list = list()
 for(i in seq_along(percent_files)){
   temp_df <- percent_df_list[[percent_files[i]]]
-  temp_df <- temp_df[temp_df$group_num == 'group_2', ]
+  temp_df <- temp_df[temp_df$group_num == 'group_1', ]
   temp_df <- temp_df[temp_df$k == '31', ]
-  temp_df[,"database_size"] <- as.factor(strsplit(strsplit(percent_files[i], "_")[[1]],".csv")[[2]])
-  anthracis_subset_list[[percent_files[i]]] <- temp_df
+  subset_list[[percent_files[i]]] <- temp_df
 }
-anthracis_subset <- Reduce(function(x, y) merge(x, y, all=TRUE), anthracis_subset_list)
+subset <- Reduce(function(x, y) merge(x, y, all=TRUE), subset_list)
 
-make_subset_across_plot(anthracis_subset, "B. Anthracis")
+
+p_range <- make_subset_across_plot(subset, "B. Cereus")
+p_unique <-make_subset_unique_across_plot(subset, "B. Cereus")
+print(p_unique)
+output_name <- paste(working_dir, "cereus_unique.jpeg", sep="")
+ggsave(output_name, plot=p_unique, dpi=800, device="jpeg", width=10, height=8)
+
+output_name <- paste(working_dir, "cereus_unique.pdf", sep="")
+ggsave(output_name, plot=p_unique, dpi=800, device="pdf", width=10, height=8)
 
 
